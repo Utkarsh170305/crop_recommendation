22f9f447686670909f45a498913cce8fcf9acebf
Utkarsh170305 <163173858+Utkarsh170305@users.noreply.github.com>
2025-04-28 21:23:18 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas
+import sklearn
+import pickle
+
+# importing model
+model = pickle.load(open('model.pkl','rb'))
+sc = pickle.load(open('standscaler.pkl','rb'))
+ms = pickle.load(open('minmaxscaler.pkl','rb'))
+
+# creating flask app
+app = Flask(__name__)
